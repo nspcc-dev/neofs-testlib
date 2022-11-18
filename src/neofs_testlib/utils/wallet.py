7ad1817e811cb2936d73343d6cd76181c7c7c88a
a.chetaev <alex.chetaev@gmail.com>
2022-11-18 23:55:33 +0300

    Add fucntion for get first address from wallet

--- src/neofs_testlib/utils/wallet.py
+++ src/neofs_testlib/utils/wallet.py
@@ -3,6 +3,20 @@ import logging
 
 logger = logging.getLogger("neofs.testlib.utils")
 
+def get_first_address_from_wallet(wallet_path: str):
+    """
+    Extracting the last address from the given wallet.
+    Args:
+        wallet_path:  The path to the wallet to extract address from.
+        wallet_password: The password for the given wallet.
+    Returns:
+        The address for the wallet.
+    """
+    with open(wallet_path) as wallet_file:
+        wallet = json.loads(wallet_file.read())
+    address = wallet.get('accounts')[0]['address']
+    logger.info(f"got address: {address}")
+    return address
 
 def get_last_address_from_wallet(wallet_path: str):
     """
