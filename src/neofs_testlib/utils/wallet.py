fa26b4f8d1cef2392f7c388a8a9bf088140ceb96
a.chetaev <alex.chetaev@gmail.com>
2022-11-18 23:33:38 +0300

    Remove neo3 dependencies

--- src/neofs_testlib/utils/wallet.py
+++ src/neofs_testlib/utils/wallet.py
@@ -1,27 +1,10 @@
 import json
 import logging
 
-from neo3 import wallet as neo3_wallet
-
 logger = logging.getLogger("neofs.testlib.utils")
 
 
-def init_wallet(wallet_path: str, wallet_password: str):
-    """
-    Create new wallet and new account.
-    Args:
-        wallet_path:  The path to the wallet to save wallet.
-        wallet_password: The password for new wallet.
-    """
-    wallet = neo3_wallet.Wallet()
-    account = neo3_wallet.Account.create_new(wallet_password)
-    wallet.account_add(account)
-    with open(wallet_path, "w") as out:
-        json.dump(wallet.to_json(), out)
-    logger.info(f"Init new wallet: {wallet_path}, address: {account.address}")
-
-
-def get_last_address_from_wallet(wallet_path: str, wallet_password: str):
+def get_last_address_from_wallet(wallet_path: str):
     """
     Extracting the last address from the given wallet.
     Args:
@@ -31,7 +14,7 @@ def get_last_address_from_wallet(wallet_path: str, wallet_password: str):
         The address for the wallet.
     """
     with open(wallet_path) as wallet_file:
-        wallet = neo3_wallet.Wallet.from_json(json.load(wallet_file), password=wallet_password)
-    address = wallet.accounts[-1].address
+        wallet = json.loads(wallet_file.read())
+    address = wallet.get('accounts')[-1]['address']
     logger.info(f"got address: {address}")
     return address
