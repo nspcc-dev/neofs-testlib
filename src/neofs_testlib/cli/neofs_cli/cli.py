1191ba267280df081b5ee5bac6791f98917952c3
Vladimir Avdeev <v.avdeev@yadro.com>
2022-12-05 23:14:17 +0300

    Add neofs-cli control shards command
    Signed-off-by: Vladimir Avdeev <v.avdeev@yadro.com>

--- src/neofs_testlib/cli/neofs_cli/cli.py
+++ src/neofs_testlib/cli/neofs_cli/cli.py
@@ -6,6 +6,7 @@ from neofs_testlib.cli.neofs_cli.container import NeofsCliContainer
 from neofs_testlib.cli.neofs_cli.netmap import NeofsCliNetmap
 from neofs_testlib.cli.neofs_cli.object import NeofsCliObject
 from neofs_testlib.cli.neofs_cli.session import NeofsCliSession
+from neofs_testlib.cli.neofs_cli.shards import NeofsCliShards
 from neofs_testlib.cli.neofs_cli.storagegroup import NeofsCliStorageGroup
 from neofs_testlib.cli.neofs_cli.util import NeofsCliUtil
 from neofs_testlib.cli.neofs_cli.version import NeofsCliVersion
@@ -19,6 +20,7 @@ class NeofsCli:
     netmap: Optional[NeofsCliNetmap] = None
     object: Optional[NeofsCliObject] = None
     session: Optional[NeofsCliSession] = None
+    shards: Optional[NeofsCliShards] = None
     storagegroup: Optional[NeofsCliStorageGroup] = None
     util: Optional[NeofsCliUtil] = None
     version: Optional[NeofsCliVersion] = None
@@ -30,6 +32,7 @@ class NeofsCli:
         self.netmap = NeofsCliNetmap(shell, neofs_cli_exec_path, config=config_file)
         self.object = NeofsCliObject(shell, neofs_cli_exec_path, config=config_file)
         self.session = NeofsCliSession(shell, neofs_cli_exec_path, config=config_file)
+        self.shards = NeofsCliShards(shell, neofs_cli_exec_path, config=config_file)
         self.storagegroup = NeofsCliStorageGroup(shell, neofs_cli_exec_path, config=config_file)
         self.util = NeofsCliUtil(shell, neofs_cli_exec_path, config=config_file)
         self.version = NeofsCliVersion(shell, neofs_cli_exec_path, config=config_file)
