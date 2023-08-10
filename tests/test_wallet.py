693324c90d4bd07aaf09c281e3b5dc813ee53f5b
Evgeniy Zayats <zayatsevgeniy@nspcc.io>
2023-08-10 08:46:01 -0400

    utils: Handle wallet api changes in mamba, fixes #66
    Signed-off-by: Evgeniy Zayats <zayatsevgeniy@nspcc.io>

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -19,17 +19,17 @@ class TestWallet(TestCase):
             init_wallet(wallet_file_path, password)
             self.assertTrue(os.path.exists(wallet_file_path))
             with open(wallet_file_path, "r") as wallet_file:
-                Wallet.from_json(json.load(wallet_file), password=password)
+                Wallet.from_json(json.load(wallet_file), passwords=[password])
             with self.assertRaises(ValueError):
                 with open(wallet_file_path, "r") as wallet_file:
-                    Wallet.from_json(json.load(wallet_file), password=wrong_password)
+                    Wallet.from_json(json.load(wallet_file), passwords=[wrong_password])
             os.unlink(wallet_file_path)
 
     def test_get_last_address_from_wallet(self):
         wallet_file_path = f"{str(uuid4())}.json"
         init_wallet(wallet_file_path, self.DEFAULT_PASSWORD)
         with open(wallet_file_path, "r") as wallet_file:
-            wallet = Wallet.from_json(json.load(wallet_file), password=self.DEFAULT_PASSWORD)
+            wallet = Wallet.from_json(json.load(wallet_file), passwords=[self.DEFAULT_PASSWORD])
         last_address = wallet.accounts[-1].address
         self.assertEqual(
             get_last_address_from_wallet(wallet_file_path, self.DEFAULT_PASSWORD),
