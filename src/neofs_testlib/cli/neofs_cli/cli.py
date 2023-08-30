82c00a0b39a521cb685785a9c37e4258fbdc9e2c
Evgeniy Zayats <zayatsevgeniy@nspcc.io>
2023-08-30 16:33:32 -0400

    cli: add wrapper for bearer create command
    Signed-off-by: Evgeniy Zayats <zayatsevgeniy@nspcc.io>

--- src/neofs_testlib/cli/neofs_cli/cli.py
+++ src/neofs_testlib/cli/neofs_cli/cli.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from neofs_testlib.cli.neofs_cli.accounting import NeofsCliAccounting
 from neofs_testlib.cli.neofs_cli.acl import NeofsCliACL
+from neofs_testlib.cli.neofs_cli.bearer import NeofsCliBearer
 from neofs_testlib.cli.neofs_cli.container import NeofsCliContainer
 from neofs_testlib.cli.neofs_cli.netmap import NeofsCliNetmap
 from neofs_testlib.cli.neofs_cli.object import NeofsCliObject
@@ -16,6 +17,7 @@ from neofs_testlib.shell import Shell
 class NeofsCli:
     accounting: Optional[NeofsCliAccounting] = None
     acl: Optional[NeofsCliACL] = None
+    bearer: Optional[NeofsCliBearer] = None
     container: Optional[NeofsCliContainer] = None
     netmap: Optional[NeofsCliNetmap] = None
     object: Optional[NeofsCliObject] = None
@@ -28,6 +30,7 @@ class NeofsCli:
     def __init__(self, shell: Shell, neofs_cli_exec_path: str, config_file: Optional[str] = None):
         self.accounting = NeofsCliAccounting(shell, neofs_cli_exec_path, config=config_file)
         self.acl = NeofsCliACL(shell, neofs_cli_exec_path, config=config_file)
+        self.bearer = NeofsCliBearer(shell, neofs_cli_exec_path, config=config_file)
         self.container = NeofsCliContainer(shell, neofs_cli_exec_path, config=config_file)
         self.netmap = NeofsCliNetmap(shell, neofs_cli_exec_path, config=config_file)
         self.object = NeofsCliObject(shell, neofs_cli_exec_path, config=config_file)
